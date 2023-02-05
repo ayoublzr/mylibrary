05b2fd7139cc85bc381baebc3eb522aa4fe6fa01
[Ayoub] <[ayoublzr93@gmail.com]>
2023-02-05 18:15:45 +0100

    create server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path=require("path");
+const app = express();
+
+
+
+app.use(express.static(path.join(__dirname, "assets")))
+app.set('view engine', 'ejs');
+app.set('views','views');
+
+
+
+
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+
+
+
+
+app.listen(3000, () => console.log("listening on port 3000"));
